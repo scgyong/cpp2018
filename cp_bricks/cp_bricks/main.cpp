2f348526132a7ce0de0d603e9fa9cf970a3ed3d0
scgyong <scgyong@gmail.com>
2018-05-15 13:07:25 +0900

    Default main event loop

--- cp_bricks/cp_bricks/main.cpp
+++ cp_bricks/cp_bricks/main.cpp
@@ -5,6 +5,16 @@ using namespace sf;
 int main(void)
 {
 	RenderWindow window(VideoMode(800, 600), "Bricks");
+	window.setFramerateLimit(60);
+	while (window.isOpen()) {
+		Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == Event::Closed) {
+				window.close();
+			}
+		}
+		window.display();
+	}
 	return 0;
 }
 
